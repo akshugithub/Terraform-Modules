4ba787822dfcaafa3ea8487e113d0392ab5b2ea1
Akshaya M <111352992+akshugithub@users.noreply.github.com>
2023-04-25 14:19:05 +0530

    Update variables.tf

--- module-2/variables.tf
+++ module-2/variables.tf
@@ -1,8 +1,3 @@
-variable "web_region" {
-  type        = string
-  description = "Region of Project"
-  default     = "eu-central-1"
-}
 variable "web_instance_type" {
   type        = string
   description = "Instance Type of EC2"
@@ -12,14 +7,4 @@ variable "ami_id" {
   type        = string
   description = "EC2 Machine AMI id"
   default     = "ami-0767046d1677be5a0"
-}
-variable "access_key" {
-  type        = string
-  description = "Access key of AWS"
-  default     = ""
-}
-variable "secret_key" {
-  type        = string
-  description = "Secret Access key of AWS"
-  default     = ""
 }
