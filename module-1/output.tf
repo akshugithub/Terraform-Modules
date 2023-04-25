98a0c41635c954a67564844fd009342d31e8cc13
Akshaya M <111352992+akshugithub@users.noreply.github.com>
2023-04-25 16:03:48 +0530

    Update output.tf

--- module-1/output.tf
+++ module-1/output.tf
@@ -3,6 +3,7 @@
 #  description = "Public IP address of EC2 instance"
 # }
 
- output "public_ip_ec2" {
-  value =  aws_instance.ec2_module_1.id
+ output "public_ip" {
+ description = "public ip address of first instance"
+ value       = try(aws_instance.ec2_module_1.public_ip, "")
 }
