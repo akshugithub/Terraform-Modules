645b40d851ca40f1b976420c57d438179b3cea9b
Akshaya M <111352992+akshugithub@users.noreply.github.com>
2023-04-25 16:18:49 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,18 +8,3 @@ variable "web_instance_type" {
   description = "Instance Type of EC2"
   default     = "t2.micro"
 }
-variable "ami_id" {
-  type        = string
-  description = "EC2 Machine AMI id"
-  default     = "ami-0767046d1677be5a0"
-}
- # variable "access_key" {
- #  type        = string
- #  description = "Access key of AWS"
- #  default     = ""
-# }
-# variable "secret_key" {
- # type        = string
- # description = "Secret Access key of AWS"
- # default     = ""
-# }
