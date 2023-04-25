ff039f8ae2be92fbf72456ac860fd50df438a054
Akshaya M <111352992+akshugithub@users.noreply.github.com>
2023-04-25 14:24:48 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "web_region" {
   type        = string
   description = "Region of Project"
-  default     = "eu-central-1"
+  default     = "ap-south-1"
 }
 variable "web_instance_type" {
   type        = string
@@ -13,13 +13,13 @@ variable "ami_id" {
   description = "EC2 Machine AMI id"
   default     = "ami-0767046d1677be5a0"
 }
-variable "access_key" {
-  type        = string
-  description = "Access key of AWS"
-  default     = ""
-}
-variable "secret_key" {
-  type        = string
-  description = "Secret Access key of AWS"
-  default     = ""
-}
+ # variable "access_key" {
+ #  type        = string
+ #  description = "Access key of AWS"
+ #  default     = ""
+# }
+# variable "secret_key" {
+ # type        = string
+ # description = "Secret Access key of AWS"
+ # default     = ""
+# }
